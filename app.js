2898e5096b4288b28a00b462e95f34d5f408e5f0
erez giladi <erez.giladi@hpe.com>
2016-08-09 12:14:50 +0300

    f

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('starting ' + process.env.FOO);
+console.log('starting ' + process.env.SOCK_SERVER);
 require('http')
    .createServer((_, response) =>  response.end('hello 123'))
    .listen(process.env.PORT || 8080);
