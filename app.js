8745d26f7b6711007fc2df219830dc8a8afeb325
erez giladi <erez.giladi@hpe.com>
2016-08-09 12:19:11 +0300

    f

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 console.log('starting ' + process.env.SOCK_SERVER);
+if (process.env.SOCK_SERVER) {
+   console.log('starting server');
+   var _ = require('./server.js');
+}
 // require('http')
 //    .createServer((_, response) =>  response.end('hello 123'))
 //    .listen(process.env.PORT || 8080);
